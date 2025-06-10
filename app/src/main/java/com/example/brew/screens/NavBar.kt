0721b60b6e8f9e73a16c8a418a8c51e3f9c10319
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-10 22:41:43 +0100

    NavBar file created with initial steps for the bottom nav bar layout

---
+++ app/src/main/java/com/example/brew/screens/NavBar.kt
@@ -0,0 +1,17 @@
+package com.example.brew.screens
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationRail
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BottomNavigation(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+
+    }
+}
