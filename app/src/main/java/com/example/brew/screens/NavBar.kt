d677ddb1d88a8eb17f6109bf7e92d0c9e0f78121
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-10 22:54:35 +0100

    Bottom NavBar layout mostly done. need to change the onselected colour and start the nav rail

--- app/src/main/java/com/example/brew/screens/NavBar.kt
+++ app/src/main/java/com/example/brew/screens/NavBar.kt
@@ -1,10 +1,18 @@
 package com.example.brew.screens
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.brew.ui.theme.BrewTheme
 
 @Composable
 fun BottomNavigation(modifier: Modifier = Modifier) {
@@ -12,6 +20,39 @@ fun BottomNavigation(modifier: Modifier = Modifier) {
         containerColor = MaterialTheme.colorScheme.background,
         modifier = modifier
     ) {
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Home")
+            },
+            selected = true,
+            onClick = {}
+        )
+        NavigationBarItem(
+            icon = {
+                Icon(
+                    imageVector = Icons.Default.Favorite,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text("Liked")
+            },
+            selected = false,
+            onClick = {}
+        )
+    }
+}
 
+@Preview(showBackground = true, backgroundColor = 0xFF1E1E1E)
+@Composable
+fun BottomNavigationPreview() {
+    BrewTheme {
+        BottomNavigation()
     }
 }
