53130faba79a9bb61ddec7099f01c106280dbb3e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-10 20:45:53 +0100

    CafeRow layout working

--- app/src/main/java/com/example/brew/screens/CafeRow.kt
+++ app/src/main/java/com/example/brew/screens/CafeRow.kt
@@ -7,12 +7,12 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.brew.ui.theme.BrewTheme
 
 @Composable
 fun CafeElementRow(modifier: Modifier = Modifier) {
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(horizontal = 16.dp),
         modifier = modifier
     ) {
         items(8) { item ->
@@ -21,3 +21,10 @@ fun CafeElementRow(modifier: Modifier = Modifier) {
     }
 }
 
+@Preview(showBackground = true, backgroundColor = 0xFF1E1E1E)
+@Composable
+fun CafeRowPreview() {
+    BrewTheme {
+        CafeElementRow()
+    }
+}
