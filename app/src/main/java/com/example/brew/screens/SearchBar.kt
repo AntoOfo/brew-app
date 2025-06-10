b9b4991c3e3a258692ebe0ad818eff93d115b15e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-10 22:36:37 +0100

    search bar layout sorted

--- app/src/main/java/com/example/brew/screens/SearchBar.kt
+++ app/src/main/java/com/example/brew/screens/SearchBar.kt
@@ -1,2 +1,52 @@
 package com.example.brew.screens
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.brew.ui.theme.BrewTheme
+
+@Composable
+fun SearchBar(modifier: Modifier = Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.secondary
+            )
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedContainerColor = MaterialTheme.colorScheme.surface
+        ),
+        placeholder = {
+            Text(
+                text = "Looking for coffee?",
+                color = Color.Gray)
+        },
+        shape = MaterialTheme.shapes.extraLarge,
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
+    )
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFF1E1E1E)
+@Composable
+fun SearchBarPreview() {
+    BrewTheme {
+        SearchBar()
+    }
+}
