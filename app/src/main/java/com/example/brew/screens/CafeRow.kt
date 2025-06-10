1ffca25cb111a14a7d01b4b792e1186b869ee312
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-10 19:32:47 +0100

    CafeRow file started for implementing lazyRow for CafeElement elements

---
+++ app/src/main/java/com/example/brew/screens/CafeRow.kt
@@ -0,0 +1,23 @@
+package com.example.brew.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CafeElementRow(modifier: Modifier = Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(8) { item ->
+            CafeElement()
+        }
+    }
+}
+
