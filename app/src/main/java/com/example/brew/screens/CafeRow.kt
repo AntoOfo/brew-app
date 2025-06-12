2b41af6c1fb9fecc24f7cbfbd3195bcd9e4b3b5e
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-12 17:13:46 +0100

    cafe row also wrapped in a slightly curved surface

--- app/src/main/java/com/example/brew/screens/CafeRow.kt
+++ app/src/main/java/com/example/brew/screens/CafeRow.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -12,12 +14,18 @@ import com.example.brew.ui.theme.BrewTheme
 
 @Composable
 fun CafeElementRow(modifier: Modifier = Modifier) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        color = MaterialTheme.colorScheme.background,
         modifier = modifier.padding(horizontal = 16.dp)
     ) {
-        items(8) { item ->
-            CafeElement()
+        LazyRow(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = modifier
+        ) {
+            items(8) { item ->
+                CafeElement()
+            }
         }
     }
 }
