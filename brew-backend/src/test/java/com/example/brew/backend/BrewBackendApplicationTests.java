fe68b2f1b85133ad338aa453a8b3e240e0fc6cef
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-23 21:06:16 +0100

    spring backend folder adding to project

---
+++ brew-backend/src/test/java/com/example/brew/backend/BrewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brew.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
