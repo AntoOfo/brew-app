ee0647cc12deedca2c0502390fe9cb5beaddccf9
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-23 18:07:30 +0100

    fade in animation added for cafe row

--- app/src/main/java/com/example/brew/screens/CafeRow.kt
+++ app/src/main/java/com/example/brew/screens/CafeRow.kt
@@ -1,14 +1,25 @@
 package com.example.brew.screens
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.example.brew.ui.theme.BrewTheme
 
@@ -25,8 +36,21 @@ fun CafeElementRow(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.spacedBy(16.dp),
             modifier = modifier
         ) {
-            items(8) { item ->      // dummy size for now
-                CafeElement()
+            items(8) { index ->      // dummy size for now
+                var visible by remember { mutableStateOf(false)}
+
+                val alpha by animateFloatAsState(
+                    targetValue = if (visible) 1f else 0f,
+                    animationSpec = tween(durationMillis = 250, delayMillis = index*50),
+                    label = "fadeInAnimation"
+                )
+
+                LaunchedEffect(Unit) {
+                    visible = true
+                }
+
+                CafeElement(modifier = Modifier
+                    .alpha(alpha))
             }
         }
     }
