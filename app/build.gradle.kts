ac649fd7f0baeafd088683c6f86c5f8137a87da2
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-06-24 23:03:17 +0100

    gsonconverter dependency added and put in instance file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.compose.material3:material3-window-size-class:1.3.2")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     testImplementation(libs.junit)
--- app/src/main/java/com/example/brew/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/brew/network/RetrofitInstance.kt
@@ -1,5 +1,12 @@
 package com.example.brew.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
 
+    val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("http://10.0.2.2:8080/")  // 10.0.2.2 is emulators ip
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
