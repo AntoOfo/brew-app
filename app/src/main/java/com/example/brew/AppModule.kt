e82be2ca1a7ed4b69cbdfa6e4b11ca70e1d3b183
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-03 22:10:08 +0100

    appmodule built

--- app/src/main/java/com/example/brew/AppModule.kt
+++ app/src/main/java/com/example/brew/AppModule.kt
@@ -1,13 +1,36 @@
 package com.example.brew
 
+import android.content.Context
+import androidx.room.Room
+import com.example.brew.room.BrewDatabase
+import com.example.brew.room.LikedCoffeeDao
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 // tells hilt stuff about db
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
+    // provides the dao for getting liked coffees
+    @Provides
+    fun provideLikedCoffeeDao(db: BrewDatabase): LikedCoffeeDao {
+        return db.likedCoffeeDao()
+    }
+
+    // provides room db instance
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): BrewDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            BrewDatabase::class.java,
+            "brew_db"
+        ).build()
+    }
 
 }
