4c021760055629a3444964a8e429dcdb0edafd57
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-04 22:51:11 +0100

    location request logic in mainactivity

--- app/src/main/java/com/example/brew/MainActivity.kt
+++ app/src/main/java/com/example/brew/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.brew
 
+import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresPermission
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.core.app.ActivityCompat
 import com.example.brew.screens.MyApp
@@ -32,14 +36,48 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    // brings dialog to get perms
     private fun checkLocationPerms() {
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED && ActivityCompat
                 .checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
             ) {
+            // request perms
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 10)
+        } else {
+            // perms alr given, get location
+            getLastLocation()
         }
-        return
+
+    }
+
+    // what to do with the result
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 10) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //perms granted, get location
+                getLastLocation()
+            }
+            }
+        }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun getLastLocation() {
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                // gonna put coords in viewmodel
+            } else {
+                // idk how ur location could be null lol
+                // probs have a toast
+            }
+        }
     }
 }
 
