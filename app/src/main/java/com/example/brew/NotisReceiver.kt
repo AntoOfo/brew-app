3ddc0f18b4a95c54b1a2a1bb1dedba6507930ab2
AntoOfo <148264049+AntoOfo@users.noreply.github.com>
2025-07-08 13:11:26 +0100

    notification receiver file created

---
+++ app/src/main/java/com/example/brew/NotisReceiver.kt
@@ -0,0 +1,12 @@
+package com.example.brew
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class NotisReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
